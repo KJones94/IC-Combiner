2e7870707425c68ab8f31e6dc380718ad2539fee
kjones94 <ksjones@udel.edu>
2018-06-01 08:58:16 -0700

    Added range special and range type names

---
+++ Combiner/Converters/RangeSpecialConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Combiner
+{
+	public class RangeSpecialConverter : IValueConverter
+	{
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			int rangeSpecial = (int)(double)value;
+			string result;
+			switch (rangeSpecial)
+			{
+				case 0:
+					result = "None";
+					break;
+				case 1:
+					result = "Rock";
+					break;
+				case 2:
+					result = "Water";
+					break;
+				case 3:
+					result = "Chem";
+					break;
+				default:
+					result = "??";
+					break;
+			}
+			return result;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Combiner/Converters/RangeTypeConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Combiner
+{
+	public class RangeTypeConverter : IValueConverter
+	{
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			int rangeType = (int)(double)value;
+			string result;
+			switch (rangeType)
+			{
+				case 0:
+					result = "None";
+					break;
+				case 2:
+					result = "Quill";
+					break;
+				case 8:
+					result = "Elec";
+					break;
+				case 16:
+					result = "Sonic";
+					break;
+				case 1:
+				case 256:
+					result = "Poison";
+					break;
+				default:
+					result = "??";
+					break;
+			}
+
+			return result;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
