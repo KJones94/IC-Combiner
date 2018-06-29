cdcbdb0be6052d4933a51fee3bec2a623fdb705b
kjones94 <ksjones@udel.edu>
2018-06-29 13:15:33 -0700

    A little farther on the saved creatures export.

---
+++ Combiner/XML/CreatureXMLHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Combiner.XML
+{
+	public class CreatureXMLHandler
+	{
+		public void AddCreature(Creature creature)
+		{
+
+		}
+	}
+}
