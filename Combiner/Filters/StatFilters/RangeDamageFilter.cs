1654e77252b8b23791d89778ee9df8250a780e44
kjones94 <ksjones@udel.edu>
2019-07-29 08:58:22 -0700

    Finished BuildQuery for stat filters.

--- Combiner/Filters/StatFilters/RangeDamageFilter.cs
+++ Combiner/Filters/StatFilters/RangeDamageFilter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,19 @@ namespace Combiner
 				|| creature.RangeDamage2 >= MinValue;
 
 			return isBothUnderMax && isOneOverMin;
+		}
+
+		public override Query BuildQuery()
+		{
+			var isBothUnderMaxQuery = Query.And(
+				Query.LT("RangeDamage1", MaxValue + 1),
+				Query.LT("RangeDamage2", MaxValue + 1));
+
+			var isOneOverMinQuery = Query.Or(
+				Query.GTE("RangeDamage1", MinValue),
+				Query.GTE("RangeDamage2", MinValue));
+
+			return Query.And(isBothUnderMaxQuery, isOneOverMinQuery);
 		}
 
 		public override string ToString()
--- Combiner/Filters/StatFilters/RangeDistanceFilter.cs
+++ Combiner/Filters/StatFilters/RangeDistanceFilter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,19 @@ namespace Combiner
 				|| creature.RangeMax2 >= MinValue;
 
 			return isBothUnderMax && isOneOverMin;
+		}
+
+		public override Query BuildQuery()
+		{
+			var isBothUnderMaxQuery = Query.And(
+				Query.LT("RangeMax1", MaxValue + 1),
+				Query.LT("RangeMax2", MaxValue + 1));
+
+			var isOneOverMinQuery = Query.Or(
+				Query.GTE("RangeMax1", MinValue),
+				Query.GTE("RangeMax2", MinValue));
+
+			return Query.And(isBothUnderMaxQuery, isOneOverMinQuery);
 		}
 
 		public override string ToString()
